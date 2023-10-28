69a064672ac4b3ed310a5277ece5763a2c2cf6da
jacques <jacques.junio@hotmail.com>
2023-10-27 21:12:40 -0300

    push

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
-# Create your tests here.
+# Create your tests here
+from random import randint
+
+def rand_ratio():
+    return randint(840, 900), randint(473, 573)
--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
+from utils.teste import fake_recipe
+from faker import Faker
 
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Luiz Otávio',
+        'recipes': [fake_recipe() for _ in range(10)],
 })
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'name': 'Luiz Otávio',
+        'recipe': fake_recipe(),
 })
